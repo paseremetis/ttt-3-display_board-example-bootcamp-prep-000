d98ced94aa28254dfd50f94ad62b466f37313cca
Peter Seremetis <pswriter5348@gmail.com>
2018-02-16 21:14:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,8 @@
 def display_board
-  puts ("A Tic Tac Toe Board")
+  puts "   |   |   "
+  puts "   -----------   "
+  puts "   |   |   "
+  puts "   -----------   "
+  puts "   |   |   "
+  puts "A Tic Tac Toe Board"
 end
